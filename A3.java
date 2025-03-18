882624f18ff0f7ba651a7de759f7e35d0991221d
Prajkta Paygude <145181382+prajktapaygude@users.noreply.github.com>
2025-03-18 16:30:09 +0530

    Add files via upload

---
+++ A3.java
@@ -0,0 +1,16 @@
+package SrarPattern;
+
+public class A3 {
+	public static void main(String[] args) {
+		
+		for(int i = 1; i<=5;i++)
+		{
+			for( int j = i ; j>=1; j--)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+}
