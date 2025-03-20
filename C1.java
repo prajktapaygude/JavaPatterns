05208481d97b5c5f3111a0130270fec8efcf3e05
Prajkta Paygude <145181382+prajktapaygude@users.noreply.github.com>
2025-03-20 12:37:30 +0530

    Add files via upload

---
+++ C1.java
@@ -0,0 +1,18 @@
+package SrarPattern;
+
+public class C1 {
+	public static void main(String[] args) {
+		
+		for(int i = 1 ; i<=5 ; i++)
+		{
+			for(int j = 5 ; j>= i ; j--)
+			{
+				System.out.print("*"+" ");
+				//System.out.print(i+" ");
+				//System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
