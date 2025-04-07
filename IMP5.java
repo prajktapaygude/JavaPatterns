5c9669fd5e44ba6582d2facd5af0dc6ce567ec06
Prajkta Paygude <145181382+prajktapaygude@users.noreply.github.com>
2025-04-07 15:52:55 +0530

    Add files via upload

---
+++ IMP5.java
@@ -0,0 +1,21 @@
+package SrarPattern;
+
+public class IMP5 {
+	public static void main(String[] args) {
+	
+		for(int i = 1 ; i<=5; i++)
+		{
+			for(int k = 1 ; k<=i ; k++)
+			{
+				System.out.print(" ");
+			}
+			for(int j = i ; j<=5; j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
