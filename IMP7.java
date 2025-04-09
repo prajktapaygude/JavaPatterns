b054e952660f53734f724b9308d9708a5fc9c4de
Prajkta Paygude <145181382+prajktapaygude@users.noreply.github.com>
2025-04-09 10:24:38 +0530

    Add files via upload

---
+++ IMP7.java
@@ -0,0 +1,25 @@
+
+package SrarPattern;
+
+public class IMP7 {
+    public static void main(String[] args) {
+        
+    	int n = 9;
+    	for(int i = 0 ; i<=n ;i++)
+    	{
+    		for(int j=0; j<=n ;j++)
+    		{
+    			
+    			if(i == j || i+j==n)
+    			{
+    				System.out.print("*");
+    			}
+    			else
+    			{
+    				System.out.print(" ");
+    			}
+    		}
+    		System.out.println();
+    	}
+    }
+}
